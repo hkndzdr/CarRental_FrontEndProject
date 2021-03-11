acd826b944a9e947df05b28987c34cc3af71031e
Hakan <hakandizdar@gmail.com>
2021-03-12 02:46:18 +0300

    @

---
+++ src/app/models/rental.ts
@@ -0,0 +1,13 @@
+export interface Rental{
+    rentalId:Number,
+    rentDate:Date,
+    returnDate:Date,
+    companyName:String,
+    firstName:String,
+    lastName:String,
+    brandName:String,
+    colorName:String,
+    carDescription:String,
+    modelYear:String,
+    dailyPrice:Number
+}
---
+++ src/app/services/rental.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { RentalResponseModel } from '../models/rentalResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RentalService {
+  apiUrl = "https://localhost:44388/api/rentals/getallrentaldto"
+  constructor(private httpClient: HttpClient) { }
+
+  getRental(): Observable<RentalResponseModel> {
+    return this.httpClient.get<RentalResponseModel>(this.apiUrl);
+  }
+}
